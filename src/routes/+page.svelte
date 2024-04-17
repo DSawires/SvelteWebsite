f70769033337c789780bea6e0ce6297f4f03e885
David Sawires <davidashraf@gmail.com>
2024-04-16 23:07:54 -0500

    Vercel tracking added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+ 
+inject({ mode: dev ? 'development' : 'production' });
+
 <h1>Welcome to my website</h1>
 <p>Visit my <a href="https://github.com/dsawires">GitHub</a> to check out some of my projects.</p>
