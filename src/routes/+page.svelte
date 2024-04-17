557a3848e2d9fd142f167470bd90a9342c8aed6d
David Sawires <davidashraf@gmail.com>
2024-04-16 23:36:18 -0500

    under dev

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,5 +5,5 @@
     inject({ mode: dev ? 'development' : 'production' });
 </script>
 
-<h1>Welcome to my website</h1>
+<h1>Welcome to my website! It's still under development so check back later. :)</h1>
 <p>Visit my <a href="https://github.com/dsawires">GitHub</a> to check out some of my projects.</p>
